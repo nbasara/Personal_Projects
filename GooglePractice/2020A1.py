9e8494406bd498478870f33350e4ec7959b2cc90
Nathan Basara <nbasara@ucsc.edu>
2020-04-18 15:38:42 -0700

    None of it is working

---
+++ GooglePractice/2020A1.py
@@ -0,0 +1,14 @@
+
+t = int(input())  # read a line for number of cases
+for i in range(1, t + 1):
+  houses = []
+  n, m = [int(s) for s in input().split(" ")]  # n number of houses, budget
+  houses = input().split(" ")
+  houses.sort()
+  total = 0
+  count = 0
+  while(total < m):
+    total = total + int(houses[count])
+    if (total <= m):
+      count += 1
+  print("Case #{}: {}".format(i, count), flush = True)
