66dd079da653ae805f90a2e991e87ce30e252461
nbasara <52301957+nbasara@users.noreply.github.com>
2020-11-15 02:21:31 -0800

    Search bar is now able to log to the console what the user is trying to search

--- ravenous/src/components/SearchBar/SearchBar.js
+++ ravenous/src/components/SearchBar/SearchBar.js
@@ -7,6 +7,9 @@ class SearchBar extends React.Component {
   constructor(props) {
     super(props)
     this.state = {term: '', location: '', sortBy: 'best_match'};
+    this.handleTermChange = this.handleTermChange.bind(this);
+    this.handleLocationChange = this.handleLocationChange.bind(this);
+    this.handleSearch = this.handleSearch.bind(this);
     this.sortByOptions = {
       'Best Match': 'best_match',
       'Highest Rated': 'rating',
@@ -26,6 +29,20 @@ class SearchBar extends React.Component {
     this.setState({ sortBy : sortByOption});
   }
 
+   handleTermChange(event) {
+    this.setState({ term: event.target.value });
+   }
+
+   
+   handleLocationChange(event) {
+    this.setState({ location: event.target.value });
+  }
+
+  handleSearch(event) {
+    this.props.searchYelp(this.state.term, this.state.location, this.state.sortBy);
+    event.preventDefault();
+  }
+
   renderSortByOptions() {
     return Object.keys(this.sortByOptions).map(sortByOption => {
       let sortByOptionValue = this.sortByOptions[sortByOption];
@@ -42,11 +59,11 @@ class SearchBar extends React.Component {
           </ul>
         </div>
         <div className="SearchBar-fields">
-          <input placeholder="Search Businesses" />
-          <input placeholder="Where?" />
+          <input onChange={this.handleTermChange} placeholder="Search Businesses" />
+          <input onChange={this.handleLocationChange} placeholder="Where?" />
         </div>
         <div className="SearchBar-submit">
-          <a>Let's Go</a>
+          <a onClick={this.handleSearch}>Let's Go</a>
         </div>
       </div>
     );
