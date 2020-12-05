f7e69c66ff1234ba4f7cc956e0e30da8be9f6e43
nbasara <52301957+nbasara@users.noreply.github.com>
2020-12-04 18:54:49 -0800

    Working on filling out information

---
+++ VC_testing_qTime/README.md
@@ -0,0 +1,9 @@
+Overall Objective:  Build a website/app that is will allow users to find ALL testing locations and be able to find the wait time for county sponsorsed drive thru testing.
+
+Tasks:
+	Build Rest API that workers will be using to update the wait time for their location.
+		What is the best way to be storing this data???? (currently python array of dictionaries)
+		Using Flask
+		
+	Build basic interface for website ......(Sort of like a mock up)
+	Build app(iOS) from the ground up that functions with API to receive wait time and locations
--- VC_testing_qTime/api/api.py
+++ VC_testing_qTime/api/api.py
@@ -1,21 +1,77 @@
-import flask
-
-app = flask.Flask(__name__)
-app.config["DEBUG"] = True
+from flask import Flask
+from flask_restful import Resource, Api
 
 
-locations = [
-	{'id': 0,
-	 'location' : 'Moorpark College',
-	 'address' : '7075 Campus Rd, Moorpark, CA 93021',
-	 'appointment' : False,
-	 'Days' : 'M-F',
- 	 'Hours' : '10AM-7PM',
-	 'wait_time' : 120,
-	 'last_updated' : '12:20:21'}
-]
+app = Flask(__name__)
+api = Api(app)
 
-@app.route('/', methods=['GET'])
-def home():
-	return "<h1>Ventura County Covid Drive Thru Testing</h1><p>Current wait time for Moorpark Colleg is 1 hour.</p>"
-app.run()
+locations = {
+    'Moorpark College' : {
+        'address': '7075 Campus Rd, Moorpark, CA 93021',
+        'days': 'M-F',
+        'hours': '10am-7pm', 
+        'appointment' : False,
+        'wait' : 24,
+        'update': '13:30',
+        'phone': None
+    },
+    'Filmore Family Medical Group' : {
+        'address': '828 W Ventura St, Fillmore, CA 93015', 
+        'days': 'Tuesday',
+        'hours': '9am-4pm', 
+        'appointment' : False,
+        'wait' : 24,
+        'update': '13:30',
+        'phone': None
+    },
+    'Santa Paula' :  {
+        'address': None, 
+        'days': 'F-T',
+        'hours': '10am-7pm', 
+        'appointment' : False,
+        'wait' : 24,
+        'update': '13:30',
+        'phone': None
+    },
+    'Academic Family Medicine Center' : {
+        'address': '300 Hillmont Ave, Ventura, CA 93003', 
+        'days': None,
+        'hours': None,  
+        'appointment' : True,
+        'wait' : 24,
+        'update': '13:30',
+        'phone': '805-652-7660'
+    },
+    'Ventura County Fairgrounds' : {
+        'address': 'W Harbor Blvd, Ventura CA 93001',
+        'days': 'M-F',
+        'hours': '9am-6pm',  
+        'appointment' : False,
+        'wait' : 24,
+        'update': '13:30',
+        'phone': None
+    },
+    'Magnolia Family Medical Center' : {
+        'address': '2240 E Gonzales Rd, Oxnard, CA 9036', 
+        'days': None,
+        'hours': None, 
+        'appointment': True,
+        'wait' : 24,
+        'update': '13:30',
+        'phone': '805-652-7660'
+        }
+
+class Locations(Resource):
+    def get(self, location_id):
+        return {location_id: loactions[location_id]}
+
+    def put(self, location_id):
+        locations[location_id] = request.form['data']
+        return {location_id: locations[location_id]}
+
+api.add_resource(Locations, '/<string:location_id>') 
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
