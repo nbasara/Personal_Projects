8cbdc759c85674d8c215b10384f4c01ef678176a
nbasara <52301957+nbasara@users.noreply.github.com>
2020-12-04 19:28:12 -0800

    All of the data is logged

--- VC_testing_qTime/api/api.py
+++ VC_testing_qTime/api/api.py
@@ -59,6 +59,60 @@ locations = {
         'wait' : 24,
         'update': '13:30',
         'phone': '805-652-7660'
+        },
+    'Oxnard performing Arts and Convention Center' : {
+        'address': '800 Hobson Way, Oxnard, CA 93030',
+        'days': 'M-F',
+        'hours': '8am-9pm',
+        'appointment': True,
+        'wait': 23,
+        'update': 23,
+        'phone': '1-888-634-1123'
+        },
+    'Las Islas Family Medical Group' : {
+        'address': '325 W Channel Islands Blvd, Oxnard, CA 93033',
+        'days': None,
+        'hours': None,
+        'appointment': True,
+        'wait': None,
+        'update': None,
+        'phone': None
+        },
+    'Oxnard College' : {
+        'address': '4000 S Rose Ave, Oxnard, CA 93033',
+        'days': 'F-T',
+        'hours': '10am-7pm',
+        'appointment': False,
+        'wait': None,
+        'update': None,
+        'phone': None
+        },
+    'Thousand Oaksf Library - Newbury Park Branch' : {
+        'address': '2331 Borchard Rd, Thousand Oaks, CA, 91320',
+        'days': 'M-F',
+        'hours': '8am-8pm',
+        'appointment': False,
+        'wait': None,
+        'update': None,
+        'phone': None,
+        },
+    'Conejo Valley Family Medical Group' : {
+        'address': '125 W Thousands Oaks Blvd, Thousand Oaks, CA 91360',
+        'days': None,
+        'hours': None,
+        'appointment': True,
+        'wait': None,
+        'update': None,
+        'Phone': '805-652-7660'
+        },
+    'Sierra Vista Family Medical Clinic' : {
+        'address': '1227 E Los Angeles Ave, Simi Valley, CA 93065',
+        'days': None,
+        'hours': None,
+        'appointment': True,
+        'wait': None,
+        'update': None,
+        'Phone': '805-652-7660'
         }
 
 class Locations(Resource):
