828aee3fb5be8d6d7fbb9aef392ba84610e4c2c9
nbasara <nbasara@ucsc.edu>
2021-03-06 22:51:57 -0800

    Default Opening Buttons added

--- CSULB/543_SemesterProject/functionPoints/app.py
+++ CSULB/543_SemesterProject/functionPoints/app.py
@@ -1,15 +1,26 @@
 from tkinter import *
 
 root = Tk()
+#setting default value for my box
+root.geometry("1000x800")
 
-#creating label widget
+menu = LabelFrame(root, borderwidth=0)
 
-myLabel1 = Label(root, text="Hello World")
-myLabel2 = Label(root, text="This is Nate and this is my project")
+File = Button(menu, text="File", padx=2, borderwidth=0)
+Edit = Button(menu, text="Edit", padx=2, borderwidth=0)
+Preferences = Button(menu, text="Preferences", padx=2, borderwidth=0)
+Metrics = Button(menu, text="Metrics", padx=2, borderwidth=0)
+Help = Button(menu, text="Help", padx=2, borderwidth=0)
 
-#putting onto screen
-myLabel1.grid(row=0, column=0)
-myLabel2.grid(row=1, column=1)
+
+File.grid(row=0, column=0)
+Edit.grid(row=0, column=1)
+Preferences.grid(row=0, column=2)
+Metrics.grid(row=0, column=3)
+Help.grid(row=0, column=4)
+
+menu.grid(row=0, column=0)
+
 
 root.mainloop()
 
