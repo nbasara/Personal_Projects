bf15deb59a26fc1fe2ea2047eb3cef230631215c
nbasara <nbasara@ucsc.edu>
2021-03-08 11:41:21 -0800

    added each input row into own class

--- CSULB/543_SemesterProject/MetricSuite/FunctionPoint/FunctionPoint.py
+++ CSULB/543_SemesterProject/MetricSuite/FunctionPoint/FunctionPoint.py
@@ -1,5 +1,10 @@
 from tkinter import *
 from tkinter import ttk
+from FunctionPoint.ExternalInputs.ExternalInputs import ExternalInputs
+from FunctionPoint.ExternalOutputs.ExternalOutputs import ExternalOutputs
+from FunctionPoint.ExternalInquiries.ExternalInquiries import ExternalInquiries
+from FunctionPoint.InternalLogicalFiles.InternalLogicalFiles import InternalLogicalFiles
+from FunctionPoint.ExternalInterfaceFiles.ExternalInterfaceFiles import ExternalInterfaceFiles
 
 class FunctionPoint:
 
@@ -27,116 +32,6 @@ class FunctionPoint:
         complexLabel = Label(parent, text="Complex")
         complexLabel.config(font=("Courier", 16))
         complexLabel.grid(row=1, column=4)
-    
-    def externalInputs(self, parent):
-        eiLabel = Label(parent, text="External Inputs")
-        eiLabel.grid(row=2, column=0, padx=10, pady=10)
-
-        eiEntry = Entry(parent, width=10, borderwidth=5)
-        eiEntry.grid(row=2, column=1, padx=10, pady=10)
-
-        complexityValue = IntVar()
-        complexityValue.set("4")
-
-        simpleRadio = Radiobutton(parent, text="3", variable=complexityValue, value=3)
-        simpleRadio.grid(row=2, column=2, padx=10, pady=10)
-        
-        averageRadio = Radiobutton(parent, text="4", variable=complexityValue, value=4)
-        averageRadio.grid(row=2, column=3, padx=10, pady=10)
-
-        complexRadio = Radiobutton(parent, text="6", variable=complexityValue, value=6)
-        complexRadio.grid(row=2, column=4, padx=10, pady=10)
-
-        calculationLabel = Label(parent, text="     ", width=10, relief="groove")
-        calculationLabel.grid(row=2, column=5, padx=10, pady=10)
-    
-    def externalOutputs(self, parent):
-        eoLabel = Label(parent, text="External Outputs")
-        eoLabel.grid(row=3, column=0, padx=10, pady=10)
-
-        eoEntry = Entry(parent, width=10, borderwidth=5)
-        eoEntry.grid(row=3, column=1, padx=10, pady=10)
-
-        complexityValue = IntVar()
-        complexityValue.set("5")
-
-        simpleRadio = Radiobutton(parent, text="4", variable=complexityValue, value=4)
-        simpleRadio.grid(row=3, column=2, padx=10, pady=10)
-        
-        averageRadio = Radiobutton(parent, text="5", variable=complexityValue, value=5)
-        averageRadio.grid(row=3, column=3, padx=10, pady=10)
-
-        complexRadio = Radiobutton(parent, text="7", variable=complexityValue, value=7)
-        complexRadio.grid(row=3, column=4, padx=10, pady=10)
-
-        calculationLabel = Label(parent, text="     ", width=10, relief="groove")
-        calculationLabel.grid(row=3, column=5, padx=10, pady=10)
-    
-    def externalInquiries(self, parent):
-        eiLabel = Label(parent, text="External Inquiries")
-        eiLabel.grid(row=4, column=0, padx=10, pady=10)
-
-        eiEntry = Entry(parent, width=10, borderwidth=5)
-        eiEntry.grid(row=4, column=1, padx=10, pady=10)
-
-        complexityValue = IntVar()
-        complexityValue.set("4")
-
-        simpleRadio = Radiobutton(parent, text="3", variable=complexityValue, value=3)
-        simpleRadio.grid(row=4, column=2, padx=10, pady=10)
-        
-        averageRadio = Radiobutton(parent, text="4", variable=complexityValue, value=4)
-        averageRadio.grid(row=4, column=3, padx=10, pady=10)
-
-        complexRadio = Radiobutton(parent, text="6", variable=complexityValue, value=6)
-        complexRadio.grid(row=4, column=4, padx=10, pady=10)
-
-        calculationLabel = Label(parent, text="     ", width=10, relief="groove")
-        calculationLabel.grid(row=4, column=5, padx=10, pady=10)
-    
-    def internalLogicalFiles(self, parent):
-        ilfLabel = Label(parent, text="Internal Logic Files")
-        ilfLabel.grid(row=5, column=0, padx=10, pady=10)
-
-        ilfEntry = Entry(parent, width=10, borderwidth=5)
-        ilfEntry.grid(row=5, column=1, padx=10, pady=10)
-
-        complexityValue = IntVar()
-        complexityValue.set("10")
-
-        simpleRadio = Radiobutton(parent, text="7", variable=complexityValue, value=7)
-        simpleRadio.grid(row=5, column=2, padx=10, pady=10)
-        
-        averageRadio = Radiobutton(parent, text="10", variable=complexityValue, value=10)
-        averageRadio.grid(row=5, column=3, padx=10, pady=10)
-
-        complexRadio = Radiobutton(parent, text="15", variable=complexityValue, value=15)
-        complexRadio.grid(row=5, column=4, padx=10, pady=10)
-
-        calculationLabel = Label(parent, text="     ", width=10, relief="groove")
-        calculationLabel.grid(row=5, column=5, padx=10, pady=10)
-
-    def externalInterfaceFiles(self, parent):
-        elfLabel = Label(parent, text="External Interface Files")
-        elfLabel.grid(row=6, column=0, padx=10, pady=10)
-
-        elfEntry = Entry(parent, width=10, borderwidth=5)
-        elfEntry.grid(row=6, column=1, padx=10, pady=10)
-
-        complexityValue = IntVar()
-        complexityValue.set("7")
-
-        simpleRadio = Radiobutton(parent, text="5", variable=complexityValue, value=5)
-        simpleRadio.grid(row=6, column=2, padx=10, pady=10)
-        
-        averageRadio = Radiobutton(parent, text="7", variable=complexityValue, value=7)
-        averageRadio.grid(row=6, column=3, padx=10, pady=10)
-
-        complexRadio = Radiobutton(parent, text="10", variable=complexityValue, value=10)
-        complexRadio.grid(row=6, column=4, padx=10, pady=10)
-
-        calculationLabel = Label(parent, text="     ", width=10, relief="groove")
-        calculationLabel.grid(row=6, column=5, padx=10, pady=10)
 
     def totalRow(self, parent):
         totalLabel = Label(parent, text="Total Count")
@@ -180,11 +75,16 @@ class FunctionPoint:
         tab = ttk.Frame(parent)
         self.titleFrame(tab)
         self.complexities(tab)
-        self.externalInputs(tab)
-        self.externalOutputs(tab)
-        self.externalInquiries(tab)
-        self.internalLogicalFiles(tab)
-        self.externalInterfaceFiles(tab)
+        ei = ExternalInputs(tab)
+        ei.displayExternalInputs()
+        eo = ExternalOutputs(tab)
+        eo.displayExternlOutputs()
+        ein = ExternalInquiries(tab)
+        ein.displayExternalInquiries()
+        ilf = InternalLogicalFiles(tab)
+        ilf.displayInternalLogicalFiles()
+        eif =  ExternalInterfaceFiles(tab)
+        eif.displayExternalInterfaceFiles()
         self.totalRow(tab)
         self.computeFP(tab)
         self.valueAdjustments(tab)
