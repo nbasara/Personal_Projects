64c590fbbece4db385afe3619038d456c0329084
nbasara <nbasara@ucsc.edu>
2021-05-16 22:45:55 -0700

    I believe it is working but possibly running out of memory on the VM switching machines

--- Homemade_Encryption/encrypter.cpp
+++ Homemade_Encryption/encrypter.cpp
@@ -2,27 +2,74 @@
 #include <string>
 #include <stdlib.h>
 #include <time.h>
+#include <cmath>
+
+//calulating (base^exp) % mod
+int power(int base, unsigned int exp, int mod){
+	//initial result
+	int res = 1;
+	base = base % mod;
+
+	while(exp > 0){
+		//if y is odd, mult x with result
+		if(exp & 1){
+			res = (res*base) % mod;
+		}
+		//y is even....y = y/2
+		exp = exp>>1;
+		base = (base*base) % mod;
+	}
+	return res; 
+}
 
 bool millerTest(int n, int d){
-	return true;
+	//pick a random intger from a in range [2, n -2]
+	int a = rand() % (n - 2) + 2;
+	int x = power(a, d, n);
+	if(x == 1 || x == (n - 1)){
+		//prime
+		return true;		
+	}
+	while(d != n - 1){
+		x = (x*x) % n;
+		d *= 2;
+		if(x == n - 1){
+			//prime
+			return true;
+		}
+		if(x == 1){
+			//composite
+			return false;
+		}
+	}
+	//composite
+	return false;
+
 }
 
 bool isPrime(int num){
 	//don't need to handle base case less then three
 	//check if even
+	//Miller-Rabin Primality Test
 	if(num % 2 == 0){
 		return false;
 	}
-	//find odd number less n
+	else if(num % 5 == 0){
+		return false;
+	}
+	//write n as 2^r*d + 1 with d odd
 	int d = num - 1;
 	while(d % 2 == 0) {
 		d = d / 2; 
 	}
-	for(int i; i < 6; i++){
-		if(millerTest(n, d)){
+	//Witnessloop k times
+	for(int i = 0; i < 4; i++){
+		if(!millerTest(num, d)){
+			//not prime
 			return false;
 		}
 	}
+	//prime
 	return true;
 }
 
@@ -30,14 +77,13 @@ int generatePrime(){
 	int n;
 
 	//generate number  from 1,000,000 to 1,000,000,000
-	n = rand() % 1000000000 + 1000000;
+	n = rand() % 1000000 + 100000;
 
 	//check if a prime number
-	if (isPrime(n)) {
-		return n;
-	} else {
-		return generatePrime(); // try to generate another
+	while(!isPrime(n)){
+		n = rand() % 1000000000 + 1000000;
 	}
+	return n;
 }
 
 int main() {
@@ -47,16 +93,14 @@ int main() {
 	srand(time(NULL));
 	
 	//get file name from the user 
-	std::cout << "Please enter the file which you would like to encrypt: "; 
-	//std::cin >> file_name;
+	//std::cout << "Please enter the file which you would like to encrypt: "; 
+	//std::cin >> file_name;155708393user@user-VirtualBox
 
 	//generate two random prime numbers
 	int prime1 = generatePrime();
+	std::cout << prime1 << std::endl;
 	int prime2 = generatePrime();
-
-	std::cout <<"These are the two random numbers generated " << prime1
-		<< "\t" << prime2 << std::endl;
-
+	std::cout << prime2 << std::endl;
 
 	return 0;
 }
