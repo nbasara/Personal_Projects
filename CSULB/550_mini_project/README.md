62954664b95b568863262f66065f65d3ce1a8ced
nbasara <nbasara@ucsc.edu>
2021-09-30 18:05:25 -0700

    Made changes to readme to provide descrition and requirements, Added starter code to mini_project.c file

--- CSULB/550_mini_project/README.md
+++ CSULB/550_mini_project/README.md
@@ -2,3 +2,19 @@
 ## by Nathan Basara
 
 Synchronization using pthreads and semaphores.
+
+## Program Description
+
+Write a C program using pthreads that creates a desired amount of threads to execute a loop.  The desired threads is given as a program parameter.  All threads will modify and display a shared value during and after the loop.
+
+## Requirements
+
+* Must validate command line parameter to make sure it is a number, not sequence of alphanumerics or assorted characters.
+
+* Be capable of functioning with large amount of threads.
+
+* Must compile using a make file.
+
+* Run without errors.
+
+* Provide sufficent comments in code to help understand code.
--- CSULB/550_mini_project/mini_project.c
+++ CSULB/550_mini_project/mini_project.c
@@ -3,6 +3,42 @@
 #include <unistd.h>
 #include <pthread.h>
 
+
+//shared variable
+int SharedValue = 0;
+
+
+/*** unAltered Code ***/
+
+//multi threaded function for thread arguement
+void * SimpleThread(int thread_num) {
+	//individual to each thread
+	//gets assigned shared value 
+	int num, val;
+
+	for(num = 0; num < 20; num++) {
+
+		if(random() > RAND_MAX/2) usleep(500);
+
+		val = SharedValue;
+
+		printf("*** thread %d sees value %d\n", thread_num, val);
+	
+		//increment value
+		SharedValue = val + 1;
+	}
+	val = SharedValue;
+	printf("Thread %d sees value %d\n", thread_num, val);
+}
+
+/*** main thread ***/
+
 int main(int argc, char* argv[]) {
+
+	//Verify arguements are withing range
+
+	//Generate that many threads
+	//Send threads to SimpleThread
+
 	return 0;
 }
